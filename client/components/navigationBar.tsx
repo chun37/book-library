e367088a2fba1be7d09a692b86c94dba0cef94ec
chun37 <chun.jb.37@gmail.com>
2022-08-15 01:49:03 +0900

    登録ボタンをナビゲーションバーに置いた

--- client/components/navigationBar.tsx
+++ client/components/navigationBar.tsx
@@ -1,13 +1,29 @@
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import Link from "next/link";
 
 const NavigationBar = () => {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div">
-            蔵書管理
-          </Typography>
+          <Link href="/">
+            <a>
+              <Typography variant="h6" component="div">
+                蔵書管理
+              </Typography>
+            </a>
+          </Link>
+          <Box sx={{ mx: "auto" }}></Box>
+          <Link href="/register">
+            <Button
+              color="inherit"
+              variant="outlined"
+              startIcon={<AddCircleOutlineOutlinedIcon />}
+            >
+              登録
+            </Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
